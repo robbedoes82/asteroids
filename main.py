85aaaab75947a98b9b0d6db935d767b27db056f3
Robin Andrianne <robbedoes@gmail.com>
2025-08-16 11:39:43 +0200

    added first ame loop

--- main.py
+++ main.py
@@ -2,9 +2,24 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
+
 
 def main():
-    print("Starting Asteroids!")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # game loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+
+    # print("Starting Asteroids!")
+    # print(f"Screen width: {SCREEN_WIDTH}")
+    # print(f"Screen height: {SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":
